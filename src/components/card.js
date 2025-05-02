7c8de85a83e3d2b7d77a2fc8dc234e7fe4c98ff4
Lexie-K <xandra.kk@yandex.ru>
2025-05-02 23:10:11 +0300

    created toggleLikeBtn, edited indexjs file

---
+++ src/components/card.js
@@ -0,0 +1,28 @@
+const cardTemplate = document.querySelector('#card-template').content;
+
+const deleteCard = cardElement => cardElement.remove();
+
+const toggleLikeButton = likeButton => {
+  if (likeButton.classList.contains('card__like-button')) {
+    likeButton.classList.toggle('card__like-button_is-active');
+  }
+};
+
+const createCard = ({ link, name }, deleteHandler, handleLike) => {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const [cardImage, cardTitle, deleteButton, likeButton] = [
+    '.card__image',
+    '.card__title',
+    '.card__delete-button',
+    '.card__like-button',
+  ].map(selector => cardElement.querySelector(selector));
+
+  cardImage.src = link;
+  cardImage.alt = name;
+  cardTitle.textContent = name;
+  deleteButton.addEventListener('click', () => deleteHandler(cardElement));
+  likeButton.addEventListener('click', e => handleLike(e.target));
+  return cardElement;
+};
+
+export { createCard, deleteCard, toggleLikeButton };
--- src/index.js
+++ src/index.js
@@ -1,29 +1,11 @@
-import {initialCards} from './scripts/cards.js';
 import './pages/index.css'
+import {initialCards} from './scripts/cards.js';
+import {createCard, deleteCard, toggleLikeButton} from './components/card.js'
 
-const cardTemplate = document.querySelector('#card-template').content;
 const cardsContainer = document.querySelector('.places__list');
 
-const deleteCard = cardElement => cardElement.remove();
-
-const createCard = ( initialCards,  deleteHandler) => {
-  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
-  const [cardImage, cardTitle, deleteButton] = [
-    '.card__image',
-    '.card__title',
-    '.card__delete-button',
-  ].map(selector => cardElement.querySelector(selector));
-
-  cardImage.src = initialCards.link;
-  cardImage.alt = initialCards.name;
-  cardTitle.textContent = initialCards.name;
-  deleteButton.addEventListener('click', () => deleteHandler(cardElement));
-
-  return cardElement;
-};
-
 const renderCards = cards => {
-  cardsContainer.append(...cards.map(card => createCard(card, deleteCard)));
+  cardsContainer.append(...cards.map(card => createCard(card, deleteCard, toggleLikeButton)));
 };
 
 document.addEventListener('DOMContentLoaded', () => renderCards(initialCards));
